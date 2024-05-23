8a1bbafe2d52ece1caaea7cd37952c542b129a5b
sjtemp62 <sjtemp62@gmail.com>
2024-05-24 08:24:33 +0900

    forever starter settings

---
+++ start-dev.js
@@ -0,0 +1,16 @@
+// start-dev.js
+const { exec } = require('child_process');
+
+const dev = exec('npm run dev');
+
+dev.stdout.on('data', (data) => {
+  console.log(data);
+});
+
+dev.stderr.on('data', (data) => {
+  console.error(data);
+});
+
+dev.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
