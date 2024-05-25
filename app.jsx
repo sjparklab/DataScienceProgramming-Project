00a8a2377da5d459859e3760ad1d6dc3e07b3a7a
sjtemp62 <sjtemp62@gmail.com>
2024-05-25 17:07:15 +0900

    fixing test-1

--- app.jsx
+++ app.jsx
@@ -35,7 +35,7 @@ function Root() {
   useEffect(() => {
     const fetchGeoJson = async () => {
       try {
-        const response = await fetch('/geojson');
+        const response = await fetch('http://localhost:3001/geojson');
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
