d70071518b1ea7ee1ab61b5cf12b9a5094cc5bd5
sjtemp62 <sjtemp62@gmail.com>
2024-05-26 16:40:14 +0900

    Revert "sigungu added test"
    This reverts commit 69c602f3acf328f66af4fce2294fa891e168d060.

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cors({
 
 app.use(express.json());
 
-const geojsonTownPath = path.join(__dirname, 'updated_merged_data_final.geojson');
+const geojsonTownPath = path.join(__dirname, 'all_data_with_geojson_data.geojson');
 const geojsonCityPath = path.join(__dirname, 'updated_merged_data_final.geojson');
 
 const getComputedGeoJson = (geojsonData) => {
