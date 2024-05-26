fcc9ad0d061d6dcfcb49e3224eb2adc73ce55253
sjtemp62 <sjtemp62@gmail.com>
2024-05-26 09:33:56 +0900

    sigungu added test

--- server.js
+++ server.js
@@ -6,37 +6,31 @@ const app = express();
 
 const allowedOrigins = ['http://sjpark-dev.com:5173', 'https://sjpark-dev.com:5173'];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  })
-);
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 
 app.use(express.json());
 
-const geojsonPath = path.join(__dirname, 'all_data_with_geojson_data.geojson');
-const geojsonPathZoomedOut = path.join(__dirname, 'sigungu_final.geojson');
+const geojsonTownPath = path.join(__dirname, 'all_data_with_geojson_data.geojson');
+const geojsonCityPath = path.join(__dirname, '시군구_데이터통합.geojson');
 
 const getComputedGeoJson = (geojsonData, weights, statuses) => {
   const columns = [
     '2023년_계_총세대수',
     'count_transport',
     'sum_all_shop',
-    '2023년_1m2_평균_가격',
-    '2023년_평균_전세',
-    '2023년_평균_월세',
+    'montly-avg_mean'
   ];
 
   const minMaxValues = columns.reduce((acc, column) => {
-    const values = geojsonData.features.map(
-      (f) => parseFloat(f.properties[column].replace(/,/g, '')) || 0
-    );
+    const values = geojsonData.features.map(f => parseFloat(f.properties[column]) || 0);
     acc[column] = { min: Math.min(...values), max: Math.max(...values) };
     return acc;
   }, {});
@@ -46,39 +40,30 @@ const getComputedGeoJson = (geojsonData, weights, statuses) => {
     return (value - min) / (max - min);
   };
 
-  const computedValues = geojsonData.features.map((feature) => {
-    const values = columns.map(
-      (column) => parseFloat(feature.properties[column].replace(/,/g, '')) || 0
-    );
-    const normalizedValues = values.map((value, index) =>
-      normalize(value, columns[index])
-    );
+  const computedValues = geojsonData.features.map(feature => {
+    const values = columns.map(column => parseFloat(feature.properties[column]) || 0);
+    const normalizedValues = values.map((value, index) => normalize(value, columns[index]));
 
-    const averagePriceIndex =
-      (normalizedValues[3] + normalizedValues[4] + normalizedValues[5]) / 3;
-    const reverseAveragePriceIndex = 1 - averagePriceIndex;
+    const averagePriceIndex = (values[3] / 3);
+    const reverseAveragePriceIndex = 1 - normalize(averagePriceIndex, columns[3]);
 
-    const computedValue =
+    const computedValue = 
       (statuses[0] ? normalizedValues[0] * weights[0] : 0) +
       (statuses[1] ? normalizedValues[1] * weights[1] : 0) +
       (statuses[2] ? normalizedValues[2] * weights[2] : 0) +
       (statuses[3] ? reverseAveragePriceIndex * weights[3] : 0);
 
-    return computedValue;
+    feature.properties.computedValue = computedValue;
+    feature.properties.priceSumNormalized = normalizedValues[3];
+    return feature;
   });
 
-  geojsonData.features.forEach((feature, index) => {
-    feature.properties.computedValue = computedValues[index];
-  });
-
+  geojsonData.features = computedValues;
   return geojsonData;
 };
 
-app.get('/geojson', (req, res) => {
-  const zoomLevel = parseFloat(req.query.zoom) || 7;
-  const selectedPath = zoomLevel >= 9 ? geojsonPath : geojsonPathZoomedOut;
-
-  fs.readFile(selectedPath, 'utf8', (err, data) => {
+app.get('/geojson/town', (req, res) => {
+  fs.readFile(geojsonTownPath, 'utf8', (err, data) => {
     if (err) {
       res.status(500).send('GeoJSON 파일 읽기 오류');
       return;
@@ -89,13 +74,29 @@ app.get('/geojson', (req, res) => {
     const statuses = [true, true, true, true];
 
     geojsonData = getComputedGeoJson(geojsonData, weights, statuses);
+    res.json(geojsonData);
+  });
+});
 
+app.get('/geojson/city', (req, res) => {
+  fs.readFile(geojsonCityPath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('GeoJSON 파일 읽기 오류');
+      return;
+    }
+
+    let geojsonData = JSON.parse(data);
+    const weights = [1, 1, 1, 1];
+    const statuses = [true, true, true, true];
+
+    geojsonData = getComputedGeoJson(geojsonData, weights, statuses);
     res.json(geojsonData);
   });
 });
 
 app.post('/update-geojson', (req, res) => {
   const { weights, statuses } = req.body;
+  const geojsonPath = req.body.useTownData ? geojsonTownPath : geojsonCityPath;
 
   fs.readFile(geojsonPath, 'utf8', (err, data) => {
     if (err) {
@@ -104,9 +105,7 @@ app.post('/update-geojson', (req, res) => {
     }
 
     let geojsonData = JSON.parse(data);
-
     geojsonData = getComputedGeoJson(geojsonData, weights, statuses);
-
     res.json(geojsonData);
   });
 });
