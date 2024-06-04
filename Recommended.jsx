656e367f1c37dc7b3e84efb1b6c9f654d093018d
sjtemp62 <sjtemp62@gmail.com>
2024-06-04 23:21:21 +0900

    file changing

--- Recommended.jsx
+++ Recommended.jsx
@@ -3,8 +3,6 @@ import { TextField, Button, Paper, Slider, Typography, Box, MenuItem, Select, In
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { styled } from '@mui/system';
 import axios from 'axios';
-
-// JSON 데이터를 import 합니다.
 import locationsData from './locations_data.json'; // locations_data.json 파일을 동일한 디렉토리에 위치시킵니다.
 
 const RECOMMENDED_URL = import.meta.env.VITE_RECOMMENDED_URL;
@@ -44,27 +42,13 @@ const ResultContainer = styled('div')({
   marginTop: '24px',
 });
 
-// Define key-to-label mapping
 const keyLabels = {
   행정구역_x: "행정구역(행정동)명",
   '2023년_계_총세대수': "지역 내 1인가구수",
   '2023년_계_총인구수': "지역 내 총 인구수",
   '2023년_세대수(전체세대)': "지역 내 총 세대수",
-  'montly-avg_단독다가구': "단독다가구 평균 월세",
-  'dep-avg_단독다가구_월세': "단독다가구 평균 월세보증금",
-  'montly-avg_아파트': "아파트 평균 월세",
-  'dep-avg_아파트_월세': "아파트 평균 월세보증금",
-  'montly-avg_연립다세대': "연립다세대 평균 월세",
-  'dep-avg_연립다세대_월세': "연립다세대 평균 월세보증금",
-  'montly-avg_오피스텔': "오피스텔 평균 월세",
-  'dep-avg_오피스텔_월세': "오피스텔 평균 월세보증금",
-  'dep-avg_단독다가구_전세': '단독다가구 평균 전세보증금',
-  'dep-avg_아파트_전세': '아파트 평균 전세보증금',
-  'dep-avg_연립다세대_전세': '연립다세대 평균 전세보증금',
-  'dep-avg_오피스텔_전세': '오피스텔 평균 전세보증금',
-  'montly-avg_mean': "전체 월세 평균",
-  "dep-avg_rent_mean": "전체 월세보증금 평균",
-  "dep-avg_deposit_mean": "전체 전세보증금 평균",
+  '지역별 1인세대 / 지역별 전체세대': "지역별 1인세대 / 지역별 전체세대 비율",
+  '지역별 1인가구 / 전체인구비': '지역별 1인가구 / 전체인구 비율',
   "sum_과학·기술": "과학기술 상가 갯수",
   "sum_교육": "교육 상가 갯수",
   "sum_보건의료": "보건의료 상가 갯수",
@@ -79,21 +63,84 @@ const keyLabels = {
   count_bus: "버스정류장 갯수",
   count_도시철도: "도시철도 역 갯수",
   count_transport: "전체 교통수단 갯수", 
-  priceSum: '전월세가격 총 합',
+  priceSum: '면적당 전월세가격 변환 합',
   transportation: '교통규모',
   singleHousehold: '1인가구 거주규모',
   distance: '원 지점과의 거리',
   computedValue: "최종 점수"
-  // Add more key-to-label mappings as needed
 };
 
-// Create a component to display properties
 const PropertyDisplay = ({ properties }) => {
   return (
     <div>
-      {Object.entries(properties).map(([key, value]) => (
-        <Typography key={key}>{`${keyLabels[key] || key}: ${value}`}</Typography>
-      ))}
+      <Typography>{`${keyLabels['2023년_계_총세대수'] || '2023년_계_총세대수'}: ${properties['2023년_계_총세대수']}`}</Typography>
+      <Typography>{`${keyLabels["sum_all_shop"] || "sum_all_shop"}: ${properties["sum_all_shop"]}`}</Typography>
+      <Typography>{`${keyLabels["count_transport"] || "count_transport"}: ${properties["count_transport"]}`}</Typography>
+      <Typography>{`${keyLabels["priceSum"] || "priceSum"}: ${properties["priceSum"]}`}</Typography>
+      <Typography>{`원 지점과의 거리: ${properties["distance"]}`}</Typography>
+      <Typography>{`최종 점수: ${properties["computedValue"]}`}</Typography>
+
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography>추가 인구정보</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>{`${keyLabels['2023년_계_총세대수'] || '2023년_계_총세대수'}: ${properties['2023년_계_총세대수']}`}</Typography>
+          <Typography>{`${keyLabels['2023년_계_총인구수'] || '2023년_계_총인구수'}: ${properties['2023년_계_총인구수']}`}</Typography>
+          <Typography>{`${keyLabels['2023년_세대수(전체세대)'] || '2023년_세대수(전체세대)'}: ${properties['2023년_세대수(전체세대)']}`}</Typography>
+          <Typography>{`${keyLabels['지역별 1인세대 / 지역별 전체세대'] || '지역별 1인세대 / 지역별 전체세대'}: ${properties['지역별 1인세대 / 지역별 전체세대']}`}</Typography>
+          <Typography>{`${keyLabels['지역별 1인가구 / 전체인구비'] || '지역별 1인가구 / 전체인구비'}: ${properties['지역별 1인가구 / 전체인구비']}`}</Typography>
+        </AccordionDetails>
+      </Accordion>
+            
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography>추가 상가 정보</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>{`${keyLabels["sum_과학·기술"] || "sum_과학·기술"}: ${properties["sum_과학·기술"]}`}</Typography>
+          <Typography>{`${keyLabels["sum_교육"] || "sum_교육"}: ${properties["sum_교육"]}`}</Typography>
+          <Typography>{`${keyLabels["sum_보건의료"] || "sum_보건의료"}: ${properties["sum_보건의료"]}`}</Typography>
+          <Typography>{`${keyLabels["sum_부동산"] || "sum_부동산"}: ${properties["sum_부동산"]}`}</Typography>
+          <Typography>{`${keyLabels["sum_소매"] || "sum_소매"}: ${properties["sum_소매"]}`}</Typography>
+          <Typography>{`${keyLabels["sum_수리·개인"] || "sum_수리·개인"}: ${properties["sum_수리·개인"]}`}</Typography>
+          <Typography>{`${keyLabels["sum_숙박"] || "sum_숙박"}: ${properties["sum_숙박"]}`}</Typography>
+          <Typography>{`${keyLabels["sum_시설관리·임대"] || "sum_시설관리·임대"}: ${properties["sum_시설관리·임대"]}`}</Typography>
+          <Typography>{`${keyLabels["sum_예술·스포츠"] || "sum_예술·스포츠"}: ${properties["sum_예술·스포츠"]}`}</Typography>
+          <Typography>{`${keyLabels["sum_음식"] || "sum_음식"}: ${properties["sum_음식"]}`}</Typography>
+        </AccordionDetails>
+      </Accordion>
+            
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography>추가 교통수단 정보</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>{`${keyLabels["count_bus"] || "count_bus"}: ${properties["count_bus"]}`}</Typography>
+          <Typography>{`${keyLabels["count_도시철도"] || "count_도시철도"}: ${properties["count_도시철도"]}`}</Typography>
+        </AccordionDetails>
+      </Accordion>
+            
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography>추가 부동산 정보</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>{`단독다가구 월세 단위면적당 보증금: ${properties["단독다가구 월세 단위면적당 보증금"]}`}</Typography>
+          <Typography>{`단독다가구 단위면적당 월세금: ${properties["단독다가구 단위면적당 월세금"]}`}</Typography>
+          <Typography>{`단독다가구 전세 단위면적당 보증금: ${properties["단독다가구 전세 단위면적당 보증금"]}`}</Typography>
+          <Typography>{`아파트 월세 단위면적당 보증금: ${properties["아파트 월세 단위면적당 보증금"]}`}</Typography>
+          <Typography>{`아파트 단위면적당 월세금: ${properties["아파트 단위면적당 월세금"]}`}</Typography>
+          <Typography>{`아파트 전세 단위면적당 보증금: ${properties["아파트 전세 단위면적당 보증금"]}`}</Typography>
+          <Typography>{`연립다세대 월세 단위면적당 보증금: ${properties["연립다세대 월세 단위면적당 보증금"]}`}</Typography>
+          <Typography>{`연립다세대 단위면적당 월세금: ${properties["연립다세대 단위면적당 월세금"]}`}</Typography>
+          <Typography>{`연립다세대 전세 단위면적당 보증금: ${properties["연립다세대 전세 단위면적당 보증금"]}`}</Typography>
+          <Typography>{`오피스텔 월세 단위면적당 보증금: ${properties["오피스텔 월세 단위면적당 보증금"]}`}</Typography>
+          <Typography>{`오피스텔 단위면적당 월세금: ${properties["오피스텔 단위면적당 월세금"]}`}</Typography>
+          <Typography>{`오피스텔 전세 단위면적당 보증금: ${properties["오피스텔 전세 단위면적당 보증금"]}`}</Typography>
+        </AccordionDetails>
+      </Accordion>
+      
     </div>
   );
 };
@@ -146,7 +193,7 @@ function App() {
   const eupmyeondongs = currentWorkplaceSigungu ? locationsData[currentWorkplaceSido].eupmyeondongs[currentWorkplaceSigungu] : [];
 
   const filterProperties = (properties) => {
-    const excludedKeys = ['ADM_NM', 'ADM_CD', 'BASE_DATE', 'centroid', 'priceSumNormalized', 'region_code', 'reversepriceSumNormalized'];
+    const excludedKeys = ['ADM_NM', 'ADM_CD', 'BASE_DATE', 'centroid', 'priceSumNormalized', 'region_code', 'reversepriceSumNormalized', '행정구역분류', '지역별 1인가구수 / 전국 1인가구수비'];
     return Object.keys(properties)
       .filter(key => !excludedKeys.includes(key))
       .reduce((obj, key) => {
@@ -213,7 +260,7 @@ function App() {
           </FormRow>
 
           <Box>
-            <Typography gutterBottom>상업규모</Typography>
+            <Typography gutterBottom>상가 수</Typography>
             <Slider
               value={formData.commercialScale}
               onChange={handleSliderChange('commercialScale')}
@@ -227,7 +274,7 @@ function App() {
           </Box>
 
           <Box>
-            <Typography gutterBottom>교통규모</Typography>
+            <Typography gutterBottom>교통수단 접근성</Typography>
             <Slider
               value={formData.transportation}
               onChange={handleSliderChange('transportation')}
@@ -241,7 +288,7 @@ function App() {
           </Box>
 
           <Box>
-            <Typography gutterBottom>1인가구 거주규모</Typography>
+            <Typography gutterBottom>1인가구 거주수</Typography>
             <Slider
               value={formData.singleHousehold}
               onChange={handleSliderChange('singleHousehold')}
